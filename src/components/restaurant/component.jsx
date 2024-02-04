2ffe7b0aa6a07d9a8cfeb1c0362fa142e1879edd
a.semin <a.semin@vk.team>
2024-02-04 17:01:22 +0300

    Homework for lecture-5 - Create UserContext - Add login form with fake auth - Change visibility of review form depending on auth state

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -2,14 +2,17 @@ import styles from './styles.module.css';
 import {Menu} from "./parts/menu/component.jsx";
 import {Reviews} from "./parts/reviews/component.jsx";
 import {ReviewForm} from "./parts/review-form/component.jsx";
+import {useContext} from "react";
+import {UserContext} from "../../app.jsx";
 
 export const Restaurant = ({name, dishes, reviews}) => {
+    const {user, } = useContext(UserContext);
     return (
         <div className={styles.root}>
             <h3 className={styles.title}>{name}</h3>
             <Menu dishes={dishes}/>
             <Reviews source={reviews}/>
-            <ReviewForm minRating='0' maxRating='5' />
+            {user && <ReviewForm username={user.name} minRating='0' maxRating='5' />}
         </div>
     );
 }
